f3f484c91df0d13c5fa5de9619ec29e7e8d9ba7d
Subbu200456 <your_email@example.com>
2025-09-07 17:33:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { DarkModeProvider } from "./context/DarkModeContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <DarkModeProvider>
+  <BrowserRouter basename="/crm-dashboard">
     <App />
-  </DarkModeProvider>
+  </BrowserRouter>
 );
